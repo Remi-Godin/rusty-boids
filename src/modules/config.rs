83c0038b9a30fbc077fd371b3df6e34dac832ff2
regodin <regodin@pop-os.localdomain>
2023-10-02 18:53:25 -0300

    changed some of the parameters

--- src/modules/config.rs
+++ src/modules/config.rs
@@ -5,6 +5,7 @@ pub const H: f32 = 600.0;
 pub const UPDATE_RATE: f64 = 60.0;
 
 // Boids parameters
-pub const BOID_SIZE: f32 = 20.0;
+pub const BOID_SIZE: f32 = 30.0;
 pub const BOID_SPEED: f32 = 01.0;
-pub const BOID_ROT_SPEED: f32 = 1.0;
+pub const BOID_ROT_SPEED: f32 = 0.02;
+pub const BOID_NEI_DIST_THRESHOLD: f32 = 200.0;
